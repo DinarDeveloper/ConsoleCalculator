2083fe0348ea19429e37713d2ebba55862ab75ae
DinarDeveloper <spartanec_89@list.ru>
2022-10-01 12:56:10 +0300

    Добавил jar файл

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,12 @@
-import javax.script.ScriptException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         for (; ; ) {
-            //try {
-                System.out.println("Введите выражение");
-                String str = scanner.nextLine();
-                System.out.println(Calculator.getResult(str));
-           // } catch (ScriptException exception) {
-           //     System.out.println("Неверный ввод");
-           // }
+            System.out.println("Введите выражение");
+            String str = scanner.nextLine();
+            System.out.println(Calculator.getResult(str));
         }
     }
 }
